75d6b95f6be424431fa29b5910826e81560f57e1
Fernando Chicaiza <washopilot@yahoo.com>
2022-05-01 13:13:50 -0500

    anatomía de un componente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Li = ({ children }) => {
+  return <li className="clase-li">{children}</li>;
+};
+
+const App = () => {
+  const valor = 'triste';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className="clase-css">
+      <Li estado={'feliz'}>valor de li</Li>
+    </ul>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const Li = ({ children, estado, casa, edad }) => {
-  console.log(casa, edad);
-  return (
-    <li>
-      {children} estoy {estado}
-    </li>
-  );
-};
-
-const X = () => (
-  <ul>
-    <Li estado={'feliz'} casa={false} edad={24}>
-      Chanchito
-    </Li>
-    <Li estado={'triste'}>Chanchito</Li>
-    <Li estado={'emocionado'}>Felipe</Li>
-  </ul>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-ReactDOM.createRoot(document.getElementById('root')).render(<X />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
