1b14593000442743a5c4b1ff297dc30213d5d9d8
Fernando Chicaiza <washopilot@yahoo.com>
2022-05-01 18:26:39 -0500

    importando clases css tradicionales

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 // import logo from './logo.svg';
-import './App.css';
-
-const estilo2 = {
-  boxShadow: '5px 5px 5px rgba(0,0,0,1)',
-};
-
-const estilo = ({ bg = '#222' }) => ({
-  backgroundColor: bg,
-  color: '#fff',
-  padding: '10px 15px',
-  margin: '10px 15px',
-});
+// import './App.css';
+import './main.css';
 
 const Li = ({ children }) => {
-  return (
-    <li style={{ ...estilo2, ...estilo({ bg: '#333' }) }} className="clase-li">
-      {children}
-    </li>
-  );
+  return <li className="clase-li">{children}</li>;
 };
 
 const App = () => {
-  const valor = 'triste';
+  // const valor = 'triste';
   return (
-    <ul style={estilo({ bg: '#750' })} className="clase-css">
+    <ul className="clase-css">
       <Li estado={'feliz'}>valor de li</Li>
     </ul>
   );
