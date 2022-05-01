e3c53cccc65851e7ad0af27e8cc29f612ed56a3f
Fernando Chicaiza <washopilot@yahoo.com>
2022-05-01 17:54:24 -0500

    inline style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 // import logo from './logo.svg';
 import './App.css';
 
+const estilo2 = {
+  boxShadow: '5px 5px 5px rgba(0,0,0,1)',
+};
+
+const estilo = ({ bg = '#222' }) => ({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px',
+});
+
 const Li = ({ children }) => {
-  return <li className="clase-li">{children}</li>;
+  return (
+    <li style={{ ...estilo2, ...estilo({ bg: '#333' }) }} className="clase-li">
+      {children}
+    </li>
+  );
 };
 
 const App = () => {
   const valor = 'triste';
   return (
-    <ul className="clase-css">
+    <ul style={estilo({ bg: '#750' })} className="clase-css">
       <Li estado={'feliz'}>valor de li</Li>
     </ul>
   );
