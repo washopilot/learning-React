6fb569f1ac531bc34634067c443c0a408d237a10
Fernando Chicaiza <washopilot@yahoo.com>
2022-05-01 12:35:32 -0500

    dando lógica al componente

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const X = () => <p>Hola mundo</p>;
+const Li = ({ children, estado, casa, edad }) => {
+  console.log(casa, edad);
+  return (
+    <li>
+      {children} estoy {estado}
+    </li>
+  );
+};
+
+const X = () => (
+  <ul>
+    <Li estado={'feliz'} casa={false} edad={24}>
+      Chanchito
+    </Li>
+    <Li estado={'triste'}>Chanchito</Li>
+    <Li estado={'emocionado'}>Felipe</Li>
+  </ul>
+);
 ReactDOM.createRoot(document.getElementById('root')).render(<X />);
 
 // If you want to start measuring performance in your app, pass a function
