cc7e33842342a1ea3e8b2258a880d4a6ee24636e
Fernando chicaiza <washopilot@yahoo.com>
2022-05-05 10:32:43 -0500

    montando componentes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import { Component } from 'react';
+import React from 'react';
 
-class Button extends Component {
+class Button extends React.Component {
+  state = {};
+  constructor(props) {
+    super(props);
+    console.log('constructor', props);
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount de Button');
+  }
+
   render() {
     console.log('ejecutando método render de button');
     return <button>Enviar</button>;
   }
 }
 
-class App extends Component {
+class App extends React.Component {
   state = { valor: 3 };
   render() {
     console.log(this.state);
     return (
       <div>
         <p>Hola mundo</p>
-        <Button />
+        <Button chanchito="feliz" />
         <button className={`${this.state.valor}`} onClick={() => this.setState({ valor: 2 })}>
           Enviar en App
         </button>
