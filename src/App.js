d1afc9eb0bab4549d24feb3bde86dfcd71a32906
Fernando chicaiza <washopilot@yahoo.com>
2022-05-05 09:49:57 -0500

    estado de un componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Component } from 'react';
 
 class App extends Component {
+  state = { valor: 3 };
   render() {
+    console.log(this.state);
     return (
       <div>
         <p>Hola mundo</p>
+        <button className={`${this.state.valor}`} onClick={() => this.setState({ valor: 2 })}>
+          Enviar
+        </button>
       </div>
     );
   }
