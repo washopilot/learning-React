65632c64d073009b8ee9872155136bd13fd64137
Fernando Chicaiza <washopilot@yahoo.com>
2022-05-08 15:48:53 -0500

    cambiando el contexto de this

--- src/App.js
+++ src/App.js
@@ -14,18 +14,26 @@ class App extends Component {
     apellido: '',
   };
 
-  updateValues = (prop, value) => {
+  constructor(props) {
+    super(props);
+    this.updateNombre = this.updateNombre.bind(this);
+  }
+
+  updateNombre(v) {
+    console.log(this);
+    this.updateValues('nombre', v.target.value);
+  }
+
+  updateValues(prop, value) {
+    console.log(this);
     this.setState({ [prop]: value });
-  };
+  }
 
   render() {
     return (
       <p>
         Nombre completo: {`${this.state.nombre} ${this.state.apellido}`}
-        <Input
-          value={this.state.nombre}
-          onChange={(e) => this.updateValues('nombre', e.target.value)}
-        />
+        <Input value={this.state.nombre} onChange={this.updateNombre} />
         <Input
           value={this.state.apellido}
           onChange={(e) => this.updateValues('apellido', e.target.value)}
