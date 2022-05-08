6ec5bcfe2d66b841475a6e8a22bfb7fcd1c3018a
Fernando Chicaiza <washopilot@yahoo.com>
2022-05-08 12:04:11 -0500

    iniciando ejemplo para subir estado

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Button extends React.Component {
-  state = {};
-  constructor(props) {
-    super(props);
-    console.log('constructor', props);
-  }
+class Input extends Component {
+  state = { valor: '' };
 
-  componentDidMount() {
-    console.log('componentDidMount de Button');
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate de Button', prevProps, prevState);
-  }
-
-  componentWillUnmount() {
-    console.log('desmontando componente de Button', this.props, this.state);
-  }
+  handleChange = (value) => {
+    this.setState({ valor: value });
+  };
 
   render() {
-    console.log('ejecutando método render de button');
-    return <button onClick={() => this.setState({ prop: 2 })}>Enviar en Button</button>;
+    return <input value={this.state.value} onChange={(e) => this.handleChange(e.target.value)}></input>;
   }
 }
 
-class App extends React.Component {
-  state = { valor: 3 };
+class App extends Component {
   render() {
-    console.log(this.state);
     return (
-      <div>
-        <p>Hola mundo</p>
-        {this.state.valor === 3 && <Button chanchito="feliz"></Button>}
-        <button className={`${this.state.valor}`} onClick={() => this.setState({ valor: 2 })}>
-          Enviar en App
-        </button>
-      </div>
+      <p>
+        Nombre completo:
+        <Input />
+        <Input />
+      </p>
     );
   }
 }
