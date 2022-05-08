af43455ae477753e3a605f5f3d3e2ca731e48315
Fernando Chicaiza <washopilot@yahoo.com>
2022-05-08 16:36:21 -0500

    importando imágenes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    productos: [
+      { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
+      { name: 'Arbejas', price: 2500, img: '/productos/arbejas.jpg' },
+      { name: 'Tomate', price: 1500, img: '/productos/lechigas.jpg' },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <p>Hola mundo</p>
+      </div>
+    );
+  }
 }
 
 export default App;
