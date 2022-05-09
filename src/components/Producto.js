53eebc95e24536578214ce68a0e70b5cccb99a34
Fernando Chicaiza <washopilot@yahoo.com>
2022-05-08 20:34:48 -0500

    dando estilo a los productos

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
+const styles = {
+  producto: {
+    border: 'solid 1px #eee',
+    boxShadow: '0 5px 5px rgb(0,0,0,0.1)',
+    width: '30%',
+    padding: '10px 15px',
+    borderRadius: '5px',
+  },
+  img: {
+    width: '100%',
+  },
+};
+
 export default class Producto extends Component {
   render() {
     const { producto } = this.props;
     return (
-      <div>
-        <img alt={producto.name} src={producto.img} />
+      <div style={styles.producto}>
+        <img style={styles.img} alt={producto.name} src={producto.img} />
         <h3>{producto.name}</h3>
         <p>{producto.price}</p>
       </div>
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Producto from './Producto';
 
+const styles = {
+  productos: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+};
+
 export default class Productos extends Component {
   render() {
     const { productos, agregarAlCarro } = this.props;
 
     return (
-      <div>
+      <div style={styles.productos}>
         {productos.map((producto, idx) => (
           <Producto
             agregarAlCarro={agregarAlCarro}
