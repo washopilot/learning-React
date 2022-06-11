d418694708f83300f1e641af1a9dc0b91bb68a62
Fernando Chicaiza <washopilot@yahoo.com>
2022-06-10 23:35:48 -0500

    primer hook personalizado

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-  state = { contador: 0 };
-
-  incrementar = () => {
-    this.setState({ contador: this.state.contador + 1 });
+const useContador = (inicial) => {
+  const [contador, setContador] = useState(inicial);
+  const incrementar = () => {
+    setContador(contador + 1);
   };
+  return [contador, incrementar];
+};
 
-  render() {
-    return (
-      <div>
-        contador: {this.state.contador}
-        <button onClick={this.incrementar}>Incrementar</button>
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const [contador, setContador] = useState(0);
-//   return (
-//     <div>
-//       Contador:{contador}
-//       <button onClick={() => setContador(contador + 1)}>Incrementar</button>
-//     </div>
-//   );
-// };
+const App = () => {
+  const [contador, incrementar] = useContador(0);
+  return (
+    <div>
+      Contador:{contador}
+      <button onClick={incrementar}>Incrementar</button>
+    </div>
+  );
+};
 export default App;
