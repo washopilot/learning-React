afb86ae1598b7800492a7f6b82f4e1f8ee1b72dc
Fernando Chicaiza <washopilot@yahoo.com>
2022-06-10 23:29:52 -0500

    mismo contador usando clases

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { Component, useState } from 'react';
 
-const App = () => {
-  const [contador, setContador] = useState(0);
-  return (
-    <div>
-      Contador:{contador}
-      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
-    </div>
-  );
-};
+class App extends Component {
+  state = { contador: 0 };
+
+  incrementar = () => {
+    this.setState({ contador: this.state.contador + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        contador: {this.state.contador}
+        <button onClick={this.incrementar}>Incrementar</button>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   const [contador, setContador] = useState(0);
+//   return (
+//     <div>
+//       Contador:{contador}
+//       <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+//     </div>
+//   );
+// };
 export default App;
