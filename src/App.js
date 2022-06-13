4bc37a13e750c981966fca7d7241387689402d47
Fernando Chicaiza <washopilot@yahoo.com>
2022-06-12 20:48:47 -0500

    useRef caso1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import { useState, useReducer } from 'react';
-
-// const state = { contador: 0 }
-// action = {type: string, payload: any}
-
-const inicial = { contador: 10 };
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'incrementar':
-      return { contador: state.contador + 1 };
-    case 'decrementar':
-      return { contador: state.contador - 1 };
-    case 'set':
-      return { contador: action.payload };
-    default:
-      return state;
-  }
-};
+import { useRef } from 'react';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, inicial);
-  const [valor, setValor] = useState(0);
+  const ref = useRef();
+  const click = () => {
+    console.log(ref.current);
+    // ref.current.innerHTML = 'Chanchito feliz'   -- Mala práctica
+  };
+
   return (
-    <div>
-      Contador: {state.contador}
-      <input value={valor} onChange={(e) => setValor(e.target.value)} />
-      <button onClick={() => dispatch({ type: 'incrementar' })}>Más</button>
-      <button onClick={() => dispatch({ type: 'decrementar' })}>Menos</button>
-      <button onClick={() => dispatch({ type: 'set', payload: Number(valor) })}>Set</button>
+    <div onClick={click} ref={ref}>
+      lala
     </div>
   );
 };
