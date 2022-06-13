875382b9631cc5432076712a30830b607495a420
Fernando Chicaiza <washopilot@yahoo.com>
2022-06-12 22:25:26 -0500

    introducción

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+  const submit = (e) => {
+    e.preventDefault();
+    const data = Array.from(new FormData(e.target));
+    console.log(Object.fromEntries(data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={submit}>
+      <div>
+        <span>lala</span>
+        <input name="campo" />
+      </div>
+      <input name="campo-2" />
+      <input type="submit" value="Enviar" />
+    </form>
   );
-}
+};
 
 export default App;
